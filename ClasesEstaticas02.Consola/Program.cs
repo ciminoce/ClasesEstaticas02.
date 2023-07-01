dba3dceae9e64cf2697442dfa6b881a70ea140ef
ciminoce <ciminoce@yahoo.com.ar>
2023-07-01 11:26:43 -0300

    Agregar archivos de proyecto.

---
+++ ClasesEstaticas02.Consola/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClasesEstaticas02.Entidades;
+
+namespace ClasesEstaticas02.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Binaries And Decimals");
+            int nro = 2;
+            Console.WriteLine(Conversor.ConvertirDeDecimalBinario(nro));
+            string binario = "10";
+            Console.WriteLine(Conversor.ConvertirDeBinarioDecimal(binario));
+        }
+    }
+}
---
+++ ClasesEstaticas02.Entidades/Conversor.cs
@@ -0,0 +1,17 @@
+﻿namespace ClasesEstaticas02.Entidades
+{
+    /// <summary>
+    /// Clase estática para convertir de decimal a binario y viceversa
+    /// </summary>
+    public static class Conversor
+    {
+        public static string ConvertirDeDecimalBinario(int nro)
+        {
+            return Convert.ToString(nro, 2);
+        }
+        public static int ConvertirDeBinarioDecimal(string nro)
+        {
+            return Convert.ToInt32(nro, 2);
+        }
+    }
+}
